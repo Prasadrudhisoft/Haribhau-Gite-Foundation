bca8ace0c9bb4118de756606bcdedcf601642c49
samkankrale <shubham@rudhisoft.com>
2026-03-25 15:02:15 +0530

    template rendering

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Request
-
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse, HTMLResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 from admin.admin import admin
 
 
 app.include_router(admin)
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
